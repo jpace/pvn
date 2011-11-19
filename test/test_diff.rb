eca81e6ef84c7032e026025dcabd2143cf0209c0
Jeff Pace <jeugenepace@gmail.com>
2011-11-19 13:43:09 -0500

    Added diff command. Refined log command.

---
+++ test/test_diff.rb
@@ -0,0 +1,38 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+require 'rubygems'
+require 'riel'
+require 'pvn/diff'
+# require 'mockdiff'
+
+Log.level = Log::DEBUG
+Log.set_widths(-12, 4, -35)
+
+module PVN
+  class TestDiff < Test::Unit::TestCase
+    include Loggable
+
+    def uses fname
+      # @mle.file = Pathname.new(File.dirname(__FILE__) + '/files/' + fname).expand_path
+    end
+    
+    def setup
+      # @mle = MockDiffExecutor.new
+    end
+
+    def assert_diff_command exp, cmdargs = nil
+      origargs = cmdargs && cmdargs.dup
+      assert_equal exp, DiffCommand.new(:execute => false, :command_args => cmdargs, :executor => @mle).command, "arguments: " + origargs.to_s
+    end
+
+    def assert_diff_command_mock exp, cmdargs = nil
+      origargs = cmdargs && cmdargs.dup
+      assert_equal exp, DiffCommand.new(:execute => false, :command_args => cmdargs, :executor => @mle).command, "arguments: " + origargs.to_s
+    end
+
+    def test_documentation
+      doc = DiffCommand.to_doc
+      puts "doc: #{doc}".on_green
+    end
+  end
+end
---
+++ test/test_diff_doc.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+require 'rubygems'
+require 'riel'
+require 'stringio'
+require 'pvn/diff'
+
+Log.level = Log::DEBUG
+Log.set_widths(-12, 4, -35)
+
+module PVN
+  class TestDiffDoc < Test::Unit::TestCase
+    include Loggable
+
+    def test_documentation
+      strio = StringIO.new
+      DiffCommand.to_doc strio
+      puts "strio: #{strio.string}".green      
+    end
+  end
+end
