388d3a4523cbaf8951efdd37684e95334f0e79c3
Jeff Pace <jeugenepace@gmail.com>
2012-06-21 17:08:00 -0400

    More migration toward unit and integration tests.

---
+++ test/unit/tc.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'riel'
+
+require 'stringio'
+require 'test/unit'
+require 'pvn_test'
+
+# require 'pvn'
+
+module PVN
+  class TestCase < Test::Unit::TestCase
+    def setup
+    end
+    
+    def test_truth
+      assert true
+    end
+  end
+end
