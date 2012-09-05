f628b4fb46c12d360893838908f85d26abee65f9
Jeff Pace <jeugenepace@gmail.com>
2012-09-05 07:22:24 -0400

    More resource refinement.

--- test/unit/resources.rb
+++ test/unit/resources.rb
@@ -8,10 +8,9 @@ class SvnResource
   
   RES_DIR = '/proj/org/incava/pvn/test/resources/'
   
-  def initialize dir, *cmd
+  def initialize dir, cmd, args = Array.new
     @dir = dir
-    @cmd = cmd
-    info "@cmd: #{@cmd}"
+    @cmd = [ 'svn', cmd, '--xml' ] + args
   end
 
   def to_path
@@ -23,25 +22,33 @@ class SvnResource
   end
 end
 
+class WiqSvnResource < SvnResource
+  def initialize cmd, *args
+    super '/Programs/wiquery', cmd, args
+  end
+end
+
+class WiqTrSvnResource < SvnResource
+  def initialize cmd, *args
+    super '/Programs/wiquery/trunk', cmd, args
+  end
+end
+
 class Resources
   include Singleton
 
-  WIQ_TRUNK_STATUS = SvnResource.new '/Programs/wiquery/trunk', 'svn', 'status', '--xml'
+  WIQ_TRUNK_STATUS = WiqTrSvnResource.new 'status'
 
-  WIQ_LOG_L_15  = SvnResource.new '/Programs/wiquery', 'svn', 'log', '--xml', '-l', '15'
-  WIQ_LOG       = SvnResource.new '/Programs/wiquery', 'svn', 'log', '--xml'  
-  WIQ_LOG_R1    = SvnResource.new '/Programs/wiquery', 'svn', 'log', '--xml', '-r1'
-  WIQ_LOG_R1748 = SvnResource.new '/Programs/wiquery', 'svn', 'log', '--xml', '-r1748'
+  WIQ_LOG_L_15  = WiqSvnResource.new 'log', '-l', '15'
+  WIQ_LOG       = WiqSvnResource.new 'log'  
+  WIQ_LOG_R1    = WiqSvnResource.new 'log', '-r1'
+  WIQ_LOG_R1748 = WiqSvnResource.new 'log', '-r1748'
 
-  WIQTR_INFO_WIQUERY_CORE_POM_XML = SvnResource.new '/Programs/wiquery/trunk', 'svn', 'info', '--xml', 'wiquery-core/pom.xml'
-  WIQTR_INFO_POM_XML_ORIG_JAVA = SvnResource.new '/Programs/wiquery/trunk', 'svn', 'info', '--xml', 'pom.xml', 'Orig.java'
+  WIQTR_INFO_WIQUERY_CORE_POM_XML = WiqTrSvnResource.new 'info', 'wiquery-core/pom.xml'
+  WIQTR_INFO_POM_XML_ORIG_JAVA    = WiqTrSvnResource.new 'info', 'pom.xml', 'Orig.java'
 
-  WIQTR_LOG_L_15_V = SvnResource.new '/Programs/wiquery/trunk', 'svn', 'log', '--xml', '-l', '15', '-v'
-  WIQTR_LOG_POM_XML = SvnResource.new'/Programs/wiquery/trunk', 'svn', 'log', '--xml', 'pom.xml'
-
-  def svn_wiq_trunk_status
-    WIQ_TRUNK_STATUS.readlines
-  end
+  WIQTR_LOG_L_15_V = WiqTrSvnResource.new 'log', '-l', '15', '-v'
+  WIQTR_LOG_POM_XML = WiqTrSvnResource.new 'log', 'pom.xml'
 
   def generate
   end
