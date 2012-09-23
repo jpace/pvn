9784dd9f732380546fbee1152f489d886cf83711
Jeff Pace <jeugenepace@gmail.com>
2012-09-23 09:08:18 -0400

    Refactored diff tests.

---
+++ test/integration/pvn/subcommands/diff/local_differ_test.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+require 'pvn/subcommands/diff/local_differ'
+require 'integration/pvn/subcommands/diff/differ_tc'
+
+Log.level = Log::DEBUG
+
+module PVN::Subcommands::Diff
+  class LocalDifferTestCase < DifferTestCase
+
+    def create_differ opts
+      LocalDiffer.new opts
+    end
+    
+    def test_against_head
+      explines = Array.new
+
+      explines << "Index: FirstFile.txt"
+      explines << "==================================================================="
+      explines << "--- FirstFile.txt	(revision 22)"
+      explines << "+++ FirstFile.txt	(working copy)"
+      explines << "@@ -1,3 +1,4 @@"
+      explines << " this is the second line of the first file."
+      explines << " third line here."
+      explines << " fourth line this is."
+      explines << "+this is the fifth line."
+      explines << "Index: SeventhFile.txt"
+      explines << "==================================================================="
+      explines << "--- SeventhFile.txt	(revision 0)"
+      explines << "+++ SeventhFile.txt	(revision 0)"
+      explines << "@@ -0,0 +1 @@"
+      explines << "+this is the 7th file."
+      explines << "Index: dirzero/SixthFile.txt"
+      explines << "==================================================================="
+      explines << "--- dirzero/SixthFile.txt	(revision 22)"
+      explines << "+++ dirzero/SixthFile.txt	(working copy)"
+      explines << "@@ -1,2 +0,0 @@"
+      explines << "-line one."
+      explines << "-line two."
+      explines << "Index: src/ruby/dog.rb"
+      explines << "==================================================================="
+      explines << "--- src/ruby/dog.rb	(revision 0)"
+      explines << "+++ src/ruby/dog.rb	(revision 0)"
+      explines << "@@ -0,0 +1,7 @@"
+      explines << "+#!/usr/bin/ruby -w"
+      explines << "+# -*- ruby -*-"
+      explines << "+"
+      explines << "+require 'rubygems'"
+      explines << "+require 'riel'"
+      explines << "+"
+      explines << "+puts \"hello, world\""
+
+      assert_diff_command %w{ }, explines
+    end
+  end
+end
