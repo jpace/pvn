9d2da07f75fff4277b41011a2013b83d022dc668
Jeff Pace <jeugenepace@gmail.com>
2012-10-02 20:43:08 -0400

    Moved status/log path diffing to ChangedPaths.

---
+++ lib/pvn/diff/changed_paths.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+require 'pvn/io/element'
+require 'pvn/diff/options'
+require 'pvn/diff/differ'
+require 'pvn/diff/log_paths'
+require 'pvn/diff/status_paths'
+require 'pvn/diff/revision'
+
+module PVN::Diff
+  # represents both LogPaths and StatusPaths
+  class ChangedPaths
+    include Loggable
+
+    def initialize paths
+      @paths = paths
+    end
+
+    def diff_revision_to_working_copy fromrev, revision, whitespace
+      rev = PVN::Diff::RevisionRange.new nil, [ revision.to_s, 'HEAD' ]
+      logpaths = LogPaths.new rev, @paths
+      name_to_logpath = logpaths.to_map
+
+      changed_paths = Hash.new { |h, k| h[k] = Array.new }
+
+      name_to_logpath.sort.each do |name, logpath|
+        info "name: #{name}"
+        info "logpath: #{logpath}".cyan
+        if logpath.is_revision_later_than? fromrev
+          info "name: #{name}".bold.green
+          info "logpath: #{logpath}".green
+          changed_paths[name] << logpath
+        end
+      end
+
+      # info "changed_paths: #{changed_paths}".blue
+
+      statuspaths = StatusPaths.new revision, @paths
+      name_to_statuspath = statuspaths.to_map
+
+      name_to_statuspath.each do |name, statuspath|
+        changed_paths[name] << statuspath
+      end
+
+      changed_paths.sort.each do |name, paths|
+        info "name: #{name}".bold.yellow
+        paths.each do |path|
+          info "path: #{path}".yellow
+        end
+        # diff_paths paths
+      end
+    end
+
+    def diff_status_path statuspath, logpath
+    end
+  end
+end
--- lib/pvn/diff/repository_differ.rb
+++ lib/pvn/diff/repository_differ.rb
@@ -6,6 +6,7 @@ require 'pvn/diff/options'
 require 'pvn/diff/differ'
 require 'pvn/diff/log_paths'
 require 'pvn/diff/status_paths'
+require 'pvn/diff/changed_paths'
 require 'pvn/diff/revision'
 
 module PVN::Diff
@@ -42,49 +43,12 @@ module PVN::Diff
 
       # this indicates that this should be split into two classes:
       if @revision.working_copy?
-        diff_revision_to_working_copy paths
+        chgpaths = ChangedPaths.new paths
+        chgpaths.diff_revision_to_working_copy @fromrev, @revision, @whitespace
       else
         logpaths = LogPaths.new @revision, paths
         logpaths.diff_revision_to_revision @fromrev, @revision, @whitespace
       end
-    end
-
-    def diff_revision_to_working_copy paths
-      rev = PVN::Diff::RevisionRange.new nil, [ @revision.to_s, 'HEAD' ]
-      logpaths = LogPaths.new rev, paths
-      name_to_logpath = logpaths.to_map
-
-      changed_paths = Hash.new { |h, k| h[k] = Array.new }
-
-      name_to_logpath.sort.each do |name, logpath|
-        info "name: #{name}"
-        info "logpath: #{logpath}".cyan
-        if logpath.is_revision_later_than? @fromrev
-          info "name: #{name}".bold.green
-          info "logpath: #{logpath}".green
-          changed_paths[name] << logpath
-        end
-      end
-
-      # info "changed_paths: #{changed_paths}".blue
-
-      statuspaths = StatusPaths.new @revision, paths
-      name_to_statuspath = statuspaths.to_map
-
-      name_to_statuspath.each do |name, statuspath|
-        changed_paths[name] << statuspath
-      end
-
-      changed_paths.sort.each do |name, paths|
-        info "name: #{name}".bold.yellow
-        paths.each do |path|
-          info "path: #{path}".yellow
-        end
-        # diff_paths paths
-      end
-    end
-
-    def diff_status_path statuspath, logpath
     end
   end
 end
