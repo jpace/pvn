2131ff9937cb144bcecb2c98b645834f341abccc
Jeff Pace <jeugenepace@gmail.com>
2012-10-19 09:56:14 -0400

    Added seek and match.

--- lib/pvn/seek/command.rb
+++ lib/pvn/seek/command.rb
@@ -37,20 +37,58 @@ module PVN::Seek
 
       # can handle only one path for now
 
-      path = paths[0]
+      @path = paths[0]
       
       # should I embed glark in pvn?
 
-      entries = find_log_entries path
-      info "entries: #{entries}"
-      entries.each do |entry|
-        info "entry: #{entry}"
+      entries = find_log_entries @path
+      seek entries, pattern, 0, entries.size
+    end
+
+    def matches? contents, pattern
+      contents.each do |line|
+        # info "line: #{line}".cyan
+        if line.index pattern
+          info "line: #{line}".red
+          return line
+        end
       end
+      false
     end
 
     # here is the seekiness, a 40 year old algorithm
     def seek entries, pattern, from, to
+      info "from: #{from}".cyan
+      info "to: #{to}".cyan
       
+      return nil if from >= to
+      
+      midpt = from + (to - from) / 2
+      info "midpt: #{midpt}".cyan
+
+      entry = entries[midpt]
+      info "entry: #{entry.class}"
+
+      # contents = SVNx::Cat::Command 
+
+      info "path: #{@path}"
+      info "revision: #{entry.revision}"
+      catargs = SVNx::CatCommandArgs.new :path => @path, :use_cache => false, :revision => entry.revision
+      cmd = SVNx::CatCommand.new catargs
+      contents = cmd.execute
+      # info "contents: #{contents}".yellow
+
+      if line = matches?(contents, pattern)
+        if midpt == from + 1
+          # we just found the precise moment
+          info "line: #{line}".on_red
+        else
+          # go back 
+          seek entries, pattern, from, midpt + 1
+        end
+      else
+        seek entries, pattern, midpt + 1, to
+      end
     end
 
     ### $$$ this is sliced from Log::Command, from which many options will apply
